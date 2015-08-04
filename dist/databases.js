f7e38e743bb788f3875878af340e7e93b3614510
Will Jones <the8bitsquid@gmail.com>
2015-08-04 15:35:16 -0500

    Fixed custom response transform to append the default transformResponse array instead of overwriting it.

--- dist/databases.js
+++ dist/databases.js
@@ -21,12 +21,24 @@ angular.module('ualib.databases')
 /**
  * Transform the JSON response - this allows the transformed values to be cached via Angular's $resource service.
  */
-    .factory('databasesFactory', ['$resource', '$filter', 'DB_PROXY_PREPEND_URL', function($resource, $filter, DB_PROXY_PREPEND_URL){
+    .factory('databasesFactory', ['$resource', '$http', '$filter', 'DB_PROXY_PREPEND_URL', function($resource, $http, $filter, DB_PROXY_PREPEND_URL){
+
+        //TODO: centralize this function so it can be used with all apps
+        // Extend the default responseTransform array - Straight from Angular 1.2.8 API docs - https://docs.angularjs.org/api/ng/service/$http#overriding-the-default-transformations-per-request
+        function appendTransform(defaults, transform) {
+
+            // We can't guarantee that the default transformation is an array
+            defaults = angular.isArray(defaults) ? defaults : [defaults];
+            console.log(defaults.concat(transform));
+            // Append the new transformation to the defaults
+            return defaults.concat(transform);
+        }
+
         return $resource('https://wwwdev2.lib.ua.edu/databases/api/:db', {db: 'all'}, {
             cache: true,
             get: {
                 method: 'GET',
-                transformResponse: function(data){
+                transformResponse: appendTransform($http.defaults.transformResponse, function(data){
                     var db = angular.fromJson(data);
                     //Pre sort databases by title
                     var databases = $filter('orderBy')(db.databases, 'title');
@@ -56,7 +68,7 @@ angular.module('ualib.databases')
                     });
                     db.databases = databases;
                     return db;
-                }
+                })
             }
         });
     }]);
