1e77b56d93e1f6e88ba2b8b473850ae0e70435e5
Will Jones <the8bitsquid@gmail.com>
2015-12-16 18:27:50 -0600

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,61 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "index",
+      "type": "overview",
+      "moduleName": "index",
+      "shortDescription": "Databases app UI.",
+      "keywords": "api app databases default lib overview route ua ui"
+    },
+    {
+      "section": "api",
+      "id": "databases",
+      "shortName": "databases",
+      "type": "overview",
+      "moduleName": "databases",
+      "shortDescription": "Databases app UI.",
+      "keywords": "angular-filter api app databases default duscroll lib nganimate ngresource ngroute ngsanitize overview route ua ualib-ui ui ui-bootstrap"
+    },
+    {
+      "section": "api",
+      "id": "databases.constant:DB_PROXY_PREPEND_URL",
+      "shortName": "DB_PROXY_PREPEND_URL",
+      "type": "object",
+      "moduleName": "databases",
+      "shortDescription": "Proxy URL to prefix database links",
+      "keywords": "api constant database databases db_proxy_prepend_url http lib links object prefix proxy ua url"
+    },
+    {
+      "section": "api",
+      "id": "databases.Controller:DatabasesListCtrl",
+      "shortName": "DatabasesListCtrl",
+      "type": "controller",
+      "moduleName": "databases",
+      "shortDescription": "Controller for the databases route (/#/databases)",
+      "keywords": "$document $filter $location $scope a-z api array characters controller databases databasesfactory filter numalpha object route starts ui"
+    },
+    {
+      "section": "api",
+      "id": "databases.databasesFactory",
+      "shortName": "databases.databasesFactory",
+      "type": "service",
+      "moduleName": "databases",
+      "shortDescription": "Factory service to retrieve databases from the API.",
+      "keywords": "$digest $filter $http $resource allows angular angularjs api appendtransform array cached call class constant custom cycle databases databasesfactory default defaults docs extend factory function functions https initial json label label-warning list modifications org overriding-the-default-transformations-per-request performing refreshed response responsetransform retrieve returns service straight time transform transformresponse"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "api/databases",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
